7fa851caf85e86a6cab733a629c153df8f440811
japacible <japacible@gmail.com>
2014-03-24 23:07:03 -0700

    move images and kind of switch themes

---
+++ js/switch_img.js
@@ -0,0 +1,15 @@
+var TILE_THEME = ['dmah', 'baxter', 'sunjayc'];
+var LEVELS = ['2', '4', '8', '16', '32', '64', '128', 
+  '256', '512', '1024', '2048', 'super'];
+
+var changeTheme = function(value) {
+  for (var i = 0; i < LEVELS.length; i++) {
+    change(value, LEVELS[i]);
+  }
+}
+
+var change = function(name, level) {
+  var src = "../img/" + name + "-" + level + ".jpg";
+  var current = ".tile.tile-" + level + " .tile-inner";
+  $(current).css("background-image", "url(" + src + ")");
+}
