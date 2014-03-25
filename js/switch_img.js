549b394e9abf96290aa3f3d21050dfc786392ea2
japacible <japacible@gmail.com>
2014-03-25 00:12:03 -0700

    add sunjayc and few other tweaks

--- js/switch_img.js
+++ js/switch_img.js
@@ -2,15 +2,19 @@ var TILE_THEME = ['dmah', 'baxter', 'sunjayc'];
 var LEVELS = ['2', '4', '8', '16', '32', '64', '128', 
   '256', '512', '1024', '2048', 'super'];
 
-var changeTheme = function(value) {
-  console.log("change theme: " + value);
+// Changes all of the tiles given a theme
+// name: theme of the tiles
+var changeTheme = function(name) {
   for (var i = 0; i < LEVELS.length; i++) {
-    change(value, LEVELS[i]);
+    change(name, LEVELS[i]);
   }
 }
 
+// Changes the tiles of a certain number of a theme
+// name: theme of the tiles
+// level: whichever number the tile is currently at
 var change = function(name, level) {
-  console.log("change : " + name + " " + level);
+  // console.log("change : " + name + " " + level);
   var src = "../img/" + name + "-" + level + ".jpg";
   var current = ".tile.tile-" + level + " .tile-inner";
   $(current).css("background-image", "url(" + src + ")");
